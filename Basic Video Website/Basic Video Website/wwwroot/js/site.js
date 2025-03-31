cddf340182170894e591a2c858761dceaac4a970
Jayike147 <jayike.okezie@gmail.com>
2025-03-31 00:56:05 -0400

    Basic Video Website

---
+++ Basic Video Website/Basic Video Website/wwwroot/js/site.js
@@ -0,0 +1,18 @@
+﻿// script.js
+window.onload = function () {
+    const elem = document.getElementById("animate");
+    let pos = 0;
+    let direction = 1;
+    const containerWidth = document.querySelector('.container').offsetWidth;
+
+    function frame() {
+        if (pos >= containerWidth - 60 || pos <= 0) {
+            direction *= -1;
+        }
+        pos += direction * 2;
+        elem.style.left = pos + 'px';
+        requestAnimationFrame(frame);
+    }
+
+    requestAnimationFrame(frame);
+};
